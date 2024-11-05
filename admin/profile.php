e66c90faacc5b53b7ab7f60054bb1e90ac3bf76f
Adonis <75648240+Bucky-26@users.noreply.github.com>
2024-11-06 06:57:52 +0800

    sidebar

---
+++ admin/profile.php
@@ -0,0 +1,6 @@
+<?php include 'includes/header.php'; ?>
+<?php include 'includes/sidenav.php'; ?>
+
+<?php include 'includes/navbar.php'; ?>
+<?php include 'includes/footer.php'; ?> 
+<?php include 'includes/script.php'; ?>
